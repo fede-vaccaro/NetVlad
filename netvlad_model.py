00bbc6a048c002ae4524693cd128c41e4dd298bd
fede-vaccaro <federico.vaccaro@live.it>
2019-11-26 18:12:01 +0100

    added retinanet model

--- netvlad_model.py
+++ netvlad_model.py
@@ -15,7 +15,6 @@ vgg = VGG16(weights='imagenet', include_top=False, pooling='avg', input_shape=in
 
 
 class NetVLADModel:
-
     def __init__(self, layer_name='block5_conv2'):
         vgg_model = VGG16(weights='imagenet', include_top=False, pooling='avg', input_shape=input_shape)
 
@@ -29,7 +28,7 @@ class NetVLADModel:
         vgg_model.get_layer(layer_name).activation = activations.linear
         vgg_model = vis.utils.utils.apply_modifications(vgg_model)
 
-        self.base_model = vgg = Model(vgg_model.input, vgg_model.get_layer('block5_conv2').output)
+        self.base_model = Model(vgg_model.input, vgg_model.get_layer('block5_conv2').output)
         self.vgg_netvlad = None
         self.images_input = None
 
@@ -44,14 +43,12 @@ class NetVLADModel:
         self.images_input = Input(shape=(224, 224, 3))
         label_input = Input(shape=(n_classes,), name="input_label")
 
-        transpose = Permute((3, 1, 2), input_shape=(-1, 512))(self.base_model([self.images_input]))
-        embedding_size = 512
+        transpose = Permute((3, 1, 2), input_shape=(-1, 256))(self.base_model([self.images_input]))
 
         # vgg_output = vgg.output_shape[1]
-        # embedding = Dense(embedding_size, input_shape=(vgg_output,), activation='relu', name="embedding1")(vgg([images_input]))
-        reshape = Reshape((512, 14 * 14))(transpose)
+        reshape = Reshape((256, 14 * 14))(transpose)
         l2normalization = L2NormLayer()(reshape)
-        netvlad = NetVLAD(feature_size=14 * 14, max_samples=512, cluster_size=64)(
+        netvlad = NetVLAD(feature_size=14 * 14, max_samples=256, cluster_size=64)(
             l2normalization)  # , output_dim=1024)resnet_output = resnet.output_shape[1]
 
         netvlad_output = 14 * 14 * 64
@@ -93,3 +90,50 @@ class NetVLADModel:
 
     def get_netvlad_extractor(self):
         return Model(inputs=self.images_input, outputs=self.vgg_netvlad.get_layer('net_vlad_1').output)
+
+
+from keras_retinanet.keras_retinanet import models
+import os
+
+class NetVLADModelRetinaNet(NetVLADModel):
+    def __init__(self, layer_name='P4'):
+        model_path = os.path.join('keras_retinanet', 'snapshots', 'resnet50_coco_best_v2.1.0.h5')
+
+        # load retinanet model
+        model = models.load_model(model_path, backbone_name='resnet50')
+
+        # set layers untrainable
+        for layer in model.layers:
+            layer.trainable = False
+            # print(layer, layer.trainable)
+            #if layer.name is layer_name:
+            #    layer.trainable = True
+
+        custom_layer = model.get_layer(layer_name)
+        custom_layer.trainable = True
+
+        self.base_model = Model(model.input, custom_layer.output)
+
+        self.vgg_netvlad = None
+        self.images_input = None
+
+    def get_feature_extractor(self, verbose=False):
+        vgg = self.base_model
+        if verbose:
+            print(vgg.summary())
+        return vgg, vgg.output_shape
+
+
+    """ def get_feature_extractor(self, verbose=False):
+        vgg = self.base_model
+        #vgg = Model(vgg.input, vgg.get_layer('block5_conv2').output)
+        if verbose:
+            vgg.summary()
+        return vgg, vgg.output_shape
+
+    def build_netvladmodel(self, n_classes, kmeans):
+        pass
+    def get_netvlad_extractor(self):
+        return self.get_feature_extractor()
+        #return Model(inputs=self.images_input, outputs=self.vgg_netvlad.get_layer('net_vlad_1').output)
+    """
