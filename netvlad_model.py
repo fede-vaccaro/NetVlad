fixed training for softmax training

netvlad_model.py
@@ -157,13 +157,14 @@ class NetVladBase:
 
         n_classes = len(os.listdir(paths.landmark_clustered_path))
 
-        self.labels = Input(shape=(3, n_classes))
+        self.labels_ap = Input(shape=(n_classes,))
+        self.labels_n = Input(shape=(n_classes,))
 
         netvlad_a = self.netvlad_base([self.anchor])
         netvlad_p = self.netvlad_base([self.positive])
         netvlad_n = self.netvlad_base([self.negative])
-        siamese_model = Model(inputs=[self.anchor, self.positive, self.negative, self.labels],
-                              outputs=[netvlad_a, netvlad_p, netvlad_n, self.labels])
+        siamese_model = Model(inputs=[self.anchor, self.positive, self.negative, self.labels_ap, self.labels_n],
+                              outputs=[netvlad_a, netvlad_p, netvlad_n, self.labels_ap, self.labels_n])
         return siamese_model
 
     def set_mid_pca_weights(self, pca):
@@ -339,7 +340,7 @@ class GeMResnet(NetVladResnet):
         gem_out = GeM(pool_size=11)(self.base_model.get_layer(self.output_layer).output)
         gem_out = layers.Flatten()(gem_out)
         # gem_out = layers.Dense(2048, kernel_regularizer=tf.keras.regularizers.l2(0.001))(gem_out)
-        # gem_out = L2NormLayer()(gem_out)
+        gem_out = L2NormLayer()(gem_out)
         self.netvlad_base = Model(self.base_model.input, gem_out)
 
         self.siamese_model = self.get_siamese_network()

open_dataset_utils.py
@@ -455,7 +455,11 @@ class LandmarkTripletGenerator():
 
                 negatives = np.array([t[2] for t in triplets_out])
 
-                yield [anchors, positives, negatives], np.array(labels_out)  # , [y_fake]*3
+
+                labels_ap = np.array([l[0] for l in labels_out])
+                labels_n = np.array([l[2] for l in labels_out])
+
+                yield [anchors, positives, negatives], [labels_ap, labels_n]  # , [y_fake]*3
 
 
 def evaluation_triplet_generator(train_dir, netbatch_size=32, model=None):

train.py
@@ -222,9 +222,9 @@ if train:
         # set triplet loss layer softmax weights
         weights = triplet_loss_layer.get_weights()
 
-        # centroids = normalize(centroids)
+        centroids = normalize(centroids)
 
-        alpha = 50.0
+        alpha = 0.1
         assignments_weights = 2. * alpha * centroids
         assignments_bias = -alpha * np.sum(np.power(centroids, 2), axis=1)
 
@@ -303,7 +303,7 @@ if train:
 
             x, y = next(train_generator)
             # print("Starting training at epoch ", e)
-            loss_s = vgg_netvlad.train_on_batch(x + [y], None)
+            loss_s = vgg_netvlad.train_on_batch(x + y, None)
             losses_e.append(loss_s)
             description_tqdm = "Loss at epoch {0}/{3} step {1}: {2:.4f}. Lr: {4}".format(e + start_epoch, s, loss_s,
                                                                                          epochs + start_epoch, lr)