32744511183270e0afc6267bb3ea184edc9594e9
fede-vaccaro <federico.vaccaro@live.it>
2020-03-05 11:07:12 +0100

    deleted normalization

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = cuda_device
 #
 #     my_model.train_kmeans(kmeans_generator)
 
-load_means = False
+load_means = True
 train = True
 
 model = ResNet50(weights='imagenet', include_top=False, pooling='avg',
@@ -174,7 +174,8 @@ if not load_means:
         descs_array = normalize(np.array(clusters[label]))
         samples = len(descs_array)
         mean = np.sum(descs_array, axis=0)
-        mean /= np.linalg.norm(mean, ord=2)
+        # mean /= np.linalg.norm(mean, ord=2)
+        mean /= samples
         means[label] = mean
         means_h5.create_dataset(name=str(label), data=mean)
 
@@ -201,11 +202,11 @@ softmax = layers.Dense(units=len(means.keys()), activation='linear', kernel_regu
 x = softmax(x)
 
 
-training_model = Model(model.input, L2NormLayer()(x))
+training_model = Model(model.input, x)#L2NormLayer()(x))
 
-centroids = normalize(centroids)
+# centroids = normalize(centroids)
 
-alpha = 0.03
+alpha = 30.0
 assignments_weights = 2. * alpha * centroids
 assignments_bias = -alpha * np.sum(np.power(centroids, 2), axis=1)
 
@@ -244,149 +245,6 @@ if train:
     optimizer = optimizers.Adam(lr=1e-5)
     training_model.compile(loss=loss, optimizer=optimizer, metrics=['acc'])
     training_model.fit_generator(generator=train_generator, steps_per_epoch=n_steps, epochs=3, verbose=1)
-
-# print(init_generator.filepaths[0])
-# img, _ = my_utils.open_img(init_generator.filepaths[0])
-# prediction = training_model.predict(np.expand_dims(img, axis=0))
-# print(prediction)
-
-# if train:
-#     steps_per_epoch = steps_per_epoch
-#
-#     vgg_netvlad.summary()
-#
-#     start_epoch = int(args['start_epoch'])
-#     vgg_netvlad = Model(vgg_netvlad.input, TripletLossLayer(0.1)(vgg_netvlad.output))
-#
-#     lr = 1e-5
-#     cyclic = clr.CyclicLR(base_lr=1e-6, max_lr=1e-5, mode='exp_range', gamma=0.99993)
-#
-#     if model_name is not None:
-#         print("Resuming training from epoch {} at iteration {}".format(start_epoch, steps_per_epoch * start_epoch))
-#         vgg_netvlad.load_weights(model_name)
-#         # vgg_netvlad.summary()
-#
-#     opt = optimizers.Adam(lr=lr)
-#     vgg_netvlad.compile(opt)
-#
-#     steps_per_epoch_val = ceil(1491
-#                                / minibatch_size)
-#
-#     kmeans_generator = my_utils.LandmarkTripletGenerator(train_dir=paths.landmarks_path,
-#                                                          model=my_model.get_netvlad_extractor(),
-#                                                          mining_batch_size=mining_batch_size,
-#                                                          minibatch_size=minibatch_size, semi_hard_prob=semi_hard_prob,
-#                                                          threshold=threshold, use_positives_augmentation=True)
-#
-#     train_generator = kmeans_generator.generator()
-#
-#     test_generator = my_utils.evaluation_triplet_generator(paths.holidays_small_labeled_path,
-#                                                            model=my_model.get_netvlad_extractor(),
-#                                                            netbatch_size=minibatch_size)
-#
-#     losses = []
-#     val_losses = []
-#
-#     not_improving_counter = 0
-#     not_improving_thresh = 15
-#
-#     description = train_description
-#
-#     val_loss_e = []
-#
-#     for s in range(steps_per_epoch_val):
-#         x_val, _ = next(test_generator)
-#         val_loss_s = vgg_netvlad.predict_on_batch(x_val)
-#         val_loss_e.append(val_loss_s)
-#
-#     starting_val_loss = np.array(val_loss_e).mean()
-#     print("Starting validation loss: ", starting_val_loss)
-#     for e in range(epochs):
-#         t0 = time.time()
-#
-#         losses_e = []
-#
-#         pbar = tqdm(range(steps_per_epoch))
-#
-#         for s in pbar:
-#             it = K.get_value(vgg_netvlad.optimizer.iterations)
-#             if use_warm_up:
-#                 lr = utils.lr_warmup(it, min_lr=max_lr * 0.1, max_lr=max_lr, wu_steps=warm_up_steps)
-#             else:
-#                 lr = max_lr
-#
-#             K.set_value(vgg_netvlad.optimizer.lr, lr)
-#
-#             x, y = next(train_generator)
-#             # print("Starting training at epoch ", e)
-#             loss_s = vgg_netvlad.train_on_batch(x, None)
-#             losses_e.append(loss_s)
-#
-#             description_tqdm = "Loss at epoch {0}/{3} step {1}: {2:.4f}. Lr: {4}".format(e + start_epoch, s, loss_s,
-#                                                                                          epochs + start_epoch, lr)
-#             pbar.set_description(description_tqdm)
-#
-#         print("")
-#         loss = np.array(losses_e).mean()
-#         losses.append(loss)
-#
-#         val_loss_e = []
-#
-#         for s in range(steps_per_epoch_val):
-#             x_val, _ = next(test_generator)
-#             val_loss_s = vgg_netvlad.predict_on_batch(x_val)
-#             val_loss_e.append(val_loss_s)
-#
-#         val_loss = np.array(val_loss_e).mean()
-#
-#         min_val_loss = starting_val_loss
-#
-#         if e > 0:
-#             min_val_loss = np.min(val_losses)
-#
-#         val_losses.append(val_loss)
-#
-#         if val_loss < min_val_loss:
-#             model_name = "model_e{0}_{2}_{1:.4f}.h5".format(e + start_epoch, val_loss, description)
-#             print("Val. loss improved from {0:.4f}. Saving model to: {1}".format(min_val_loss, model_name))
-#             vgg_netvlad.save_weights(model_name)
-#             not_improving_counter = 0
-#         else:
-#             print("Val loss ({0:.4f}) did not improve from {1:.4f}".format(val_loss, min_val_loss))
-#             not_improving_counter += 1
-#             print("Val loss does not improve since {} epochs".format(not_improving_counter))
-#             if e % 5 == 0:
-#                 model_name = "model_e{0}_{2}_{1:.4f}_checkpoint.h5".format(e + start_epoch, val_loss, description)
-#                 vgg_netvlad.save_weights(model_name)
-#                 print("Saving model to: {} (checkpoint)".format(model_name))
-#             # if not_improving_counter == not_improving_thresh:
-#             if False:
-#                 # lr *= 0.5
-#                 # K.set_value(vgg_netvlad.optimizer.lr, lr)
-#                 # print("Learning rate set to: {}".format(lr))
-#                 opt = optimizers.Adam(lr=lr)
-#                 vgg_netvlad.compile(opt)
-#                 not_improving_counter = 0
-#                 print("Optimizer weights restarted.")
-#
-#         print("Validation loss: {}\n".format(val_loss))
-#         print("Training loss: {}\n".format(loss))
-#
-#         t1 = time.time()
-#         print("Time for epoch {}: {}s".format(e, int(t1 - t0)))
-#
-#     kmeans_generator.loader.stop_loading()
-#
-#     model_name = "model_e{}_{}_.h5".format(epochs + start_epoch, description)
-#     vgg_netvlad.save_weights(model_name)
-#     print("Saved model to disk: ", model_name)
-#
-#     plt.figure(figsize=(8, 8))
-#     plt.plot(losses, label='training loss')
-#     plt.plot(val_losses, label='validation loss')
-#     plt.legend()
-#     plt.title('Train/validation loss')
-#     plt.savefig("train_val_loss_{}.pdf".format(description))
 
 print("Testing model")
 print("Input shape: ", netvlad_model.NetVladBase.input_shape)
