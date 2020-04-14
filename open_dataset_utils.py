e64c551f6c71e182848e8b332bdf7900f1f503b5
fede-vaccaro <federico.vaccaro@live.it>
2020-04-14 18:59:06 +0200

    fix in open_dataset_utils

--- open_dataset_utils.py
+++ open_dataset_utils.py
@@ -265,7 +265,7 @@ class LandmarkTripletGenerator():
                         j_pos = j
 
                     if (j_pos is not -1) and (j_neg is not -1) and (j_pos - j_neg < self.threshold):
-                        triplet = row[0], row[j_pos], row[j_neg], anchor_label, label_list[j_neg]
+                        triplet = row[0], row[j_pos], row[j_neg], anchor_label, label_list[row[j_neg]]
 
                         d_a_p_2 = np.max((2.0 - 2.0 * np.float64(distances[i][j_pos]), 0.0))
                         d_a_n_2 = np.max((2.0 - 2.0 * np.float64(distances[i][j_neg]), 0.0))
